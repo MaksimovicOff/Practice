2bc70b937cc2416898f01fd8f7bf24739a511832
J4trie <120220993+J4trie@users.noreply.github.com>
2024-02-09 21:31:00 +0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="style/style.css">
+	<title>Авиабилеты</title>
+</head>
+<body>
+	<div class="heads">
+		<div class="header">
+			<div class="logo"></div>
+			<div class="menu">
+				<a href="">Главная</a>
+				<a href="">Отмененнные рейсы</a>
+				<a href="">Расписание</a>
+				<a href="">Отзывы</a>
+				<a href="">О нас</a>
+				<a href="">Войти</a>
+			</div>
+		</div>
+		<h1>Найти актуальные билеты</h1>
+		<div class="bilet">
+			<input type="text" name="otkuda" placeholder="Откуда">
+			<input type="text" name="kuda" placeholder="Куда">
+			<input type="date" name="data" placeholder="Дата вылета">
+			<select>
+				<option>Бизнес</option>
+				<option>Эконом</option>
+			</select>
+			<input type="submit" name="otkuda" value="Найти билет">
+		</div>
+	</div>
+	<div class="content">
+		<h2>Наши преимущества</h2>
+		<div class="preim">
+			<div class="block1">
+				<img src="img/security.png">
+				<p>Мы соблюдаем высокие стандарты безопасности, наш экипаж является высококвалифицированным</p>
+			</div>
+			<div class="block2">
+				<img src="img/like.png">
+				<p>У нас комфортабельные салоны, отличное обслуживание, которое найдет подход к каждому, есть бесплатный WI-FI</p>
+			</div>
+			<div class="block3">
+				<img src="img/three-way.png">
+				<p>Вы можете настроить свой перелет так, как вам хочется, есть гибкая настройка перелета</p>
+			</div>
+		</div>
+		<h2>Отзывы</h2>
+		<div class="otzivi">
+			<div class="otziv1">
+				<div class="menchik">
+					<img src="img/ava.png">
+					<p>Серега</p>
+				</div>
+				<p>Нормис</p>
+			</div>
+			<div class="otziv2">
+				<div class="menchik">
+					<img src="img/ava.png">
+					<p>Артем</p>
+				</div>
+				<p>Нормис</p>
+			</div>
+			<div class="otziv3">
+				<div class="menchik">
+					<img src="img/ava.png">
+					<p>Валя</p>
+				</div>
+				<p>Нормис</p>
+			</div>
+			<div class="otziv4">
+				<div class="menchik">
+					<img src="img/ava.png">
+					<p>Игорь</p>
+				</div>
+				<p>Нормис</p>
+			</div>
+		</div>
+	</div>
+	<div class="footer">
+		<div class="line">
+			<div class="logo_f"></div>
+			<div class="soc">
+				<div class="vk"><a href="#"></a></div>
+				<div class="tg"><a href="#"></a></div>
+			</div>
+		</div>
+		<div class="menu_f">
+			<a href="">Главная</a>
+			<a href="">Отмененнные рейсы</a>
+			<a href="">Расписание</a>
+			<a href="">Отзывы</a>
+			<a href="">О нас</a>
+			<a href="">Войти</a>
+		</div>
+	</div>
+</body>
+</html>
