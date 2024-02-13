52029233395d9a466d871df2c825e7d6819d2bf6
Qiawox <gv7361299@gmail.com>
2024-02-13 11:50:28 +0400

    Update js.js

--- scripts/js.js
+++ scripts/js.js
@@ -35,14 +35,59 @@ $('#set_price').click(function(){
     let adult = Number($('#Adult').val());
     let children = Number($('#Children').val());
     let price = Number($('#Price2').val());
+    let select = Number($('#class').val());
+    let role = Number($('#role').val());
+    console.log(select);
     if ($('.checkbox').is(":checked") === false) {
-        let result = price * adult + price * children * 0.8;
-        $('#Price').attr('value', result);
-        console.log('ne');
+        if (select === 1) {
+            if (role === 1) {
+                let result = price * adult + price * children * 0.8;
+                $('#Price').attr('value', result);
+                console.log('ne');
+            }
+            if (role > 1) {
+                let result = (price * adult + price * children * 0.8) * 0.8;
+                $('#Price').attr('value', result);
+                console.log('ne');  
+            }
+            
+        }if (select === 2) {
+            if (role === 1) {
+                let result = price * adult + price * children * 0.8 + price * 0.5;
+                $('#Price').attr('value', result);
+                console.log('ne');     
+            }
+            if (role > 1) {
+                let result = (price * adult + price * children * 0.8 + price * 0.5) * 0.8;
+                $('#Price').attr('value', result);
+                console.log('ne');     
+            }
+               
+        }
+        
     }else{
         if (adult > 0) {
-            let result = price * adult + price * children * 0.8 + price * 0.3;
-            $('#Price').attr('value', result);   
+            if (select === 1) {
+                if (role === 1) {
+                    let result = price * adult + price * children * 0.8 + price * 0.3;
+                    $('#Price').attr('value', result);     
+                }
+                if (role > 1) {
+                    let result = (price * adult + price * children * 0.8 + price * 0.3) * 0.8;
+                    $('#Price').attr('value', result);     
+                }  
+            }if (select === 2) {
+                if (role === 1) {
+                    let result = price * adult + price * children * 0.8 + price * 0.3 + price * 0.5;
+                    $('#Price').attr('value', result);   
+                }
+                if (role > 1) {
+                    let result = (price * adult + price * children * 0.8 + price * 0.3 + price * 0.5) * 0.8;
+                    $('#Price').attr('value', result);
+                }
+                   
+            }
+              
         }else{
         }
         
