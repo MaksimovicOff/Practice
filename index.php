a090060a2c04fece36e622b373c8e02fe4689216
Qiawox <gv7361299@gmail.com>
2024-02-15 11:58:23 +0400

    Update index.php

--- index.php
+++ index.php
@@ -16,9 +16,10 @@
 				<a href="">Расписание</a>
 				<a href="">Отзывы</a>
 				<a href="">О нас</a>
-				<a href="pages/auth.php">Войти</a>
+				<a href="#modal">Войти</a>
 			</div>
 		</div>
+		
 		<h1>Найти актуальные билеты</h1>
 		<div class="bilet">
 			<input type="text" name="otkuda" placeholder="Откуда">
@@ -31,6 +32,155 @@
 			<input type="submit" name="otkuda" value="Найти билет">
 		</div>
 	</div>
+	<div class="modal" id="modal">
+            <div class="modal_body">
+                <div class="modal_content">
+                <a href="#" class="modal_close"><img src="img/free-icon-crossed-4219073.png" alt=""></a>
+                <div class="auth-form">
+            <form method="POST">
+                <input type="email" name="email" id="" placeholder="Email" required class="modal_auth_input">
+                <div class="v_pass">
+                    <input type="password" placeholder="Password" id="password" required name="password" class="modal_auth_input">
+                    <span class="icon"></span>
+                </div>
+                <a href="">Забыли пароль?</a>
+                <input type="submit" value="Войти" name="auth">
+                <input type="submit" value="Зарегистрироваться" onclick="window.location.href='#modal_reg'">
+                <?php
+                session_start();
+                include_once 'db/db.php';
+                error_reporting(0);
+                $email = $_POST['email'];
+                $password = $_POST['password'];
+                $auth = $_POST['auth'];
+                $str_out = "SELECT * FROM `users` WHERE email = '$email' && password = '$password'";
+                $run_out = mysqli_query($connect, $str_out);
+                $num_out = mysqli_num_rows($run_out);
+                if ($_SESSION['user']['role'] == '1') {
+                    header("Location:pages/lk_user.php");
+                }elseif ($_SESSION['user']['role'] == '2') {
+                    header("Location:pages/lk_admin.php");
+                }else
+                {
+                    session_unset();
+                    if ($auth) {
+                        if ($num_out == 1) {
+                            $user = mysqli_fetch_array($run_out);
+                            $_SESSION['user'] = [
+                                "id" => $user['id'],
+                                "first_name" => $user['first_name'],
+                                "last_name" => $user['last_name'],
+                                "patronymic" => $user['patronymic'],
+                                "email" => $user['email'],
+                                "password" => $user['password'],
+                                "phone" => $user['phone'],
+                                "role" => $user['role'],
+                                "role_sales" => $user['role_sales']
+                            ];
+                            if ($_SESSION['user']['role'] == '1') {
+                                header("Location:pages/lk_user.php");
+                            }elseif ($_SESSION['user']['role'] == '2') {
+                                header("Location:pages/lk_admin.php");
+                            }
+                        }
+                        else {
+                            echo "<p class='error error_auth'>Неверный логин или пароль</p>";
+                        }
+                    }
+                }
+                ?>
+            </form>
+        </div>
+                </div>
+            </div>
+        </div>
+             <div class="modal" id="modal_reg">
+        <div class="modal_body">
+            <div class="modal_content modal_content_reg">
+            <a href="#" class="modal_close"><img src="img/free-icon-crossed-4219073.png" alt=""></a>
+            <div class="auth-form">
+            <form method="POST">
+                <input type="text" placeholder="First name" name="FirstName" required class="modal_auth_input">
+                <input type="text" placeholder="Last name" name="LastName" required class="modal_auth_input">
+                <input type="text" placeholder="Patronymic" name="Patronymic" required class="modal_auth_input">
+                <input type="email" name="Email" id="" placeholder="Email" required class="modal_auth_input">
+                <div class="v_pass">
+                    <input type="password" placeholder="Password" id="password" required name="Password" class="modal_auth_input">
+                    <span class="icon"></span>
+                </div>
+                <div class="v_pass_reply">
+                    <input type="password" placeholder="Password reply" id="password_reply" required name="ReplyPassword" class="modal_auth_input">
+                    <span class="icon_reply"></span>
+                </div>
+                <input type="text" placeholder="Phone" name="Phone" required class="modal_auth_input">
+                <input type="submit" value="Зарегистрироваться" name="Reg">
+                <input type="submit" value="Войти" onclick="window.location.href='#modal'">
+                <?php
+                    $firstname = $_POST['FirstName'];
+                    $lastname = $_POST['LastName'];
+                    $patronymic = $_POST['Patronymic'];
+                    $email = $_POST['Email'];
+                    $password = $_POST['Password'];
+                    $replypassword = $_POST['ReplyPassword'];
+                    $phone = $_POST['Phone'];
+                    $reg = $_POST['Reg'];
+
+                    $str_out_users = "SELECT * FROM `users` WHERE email = '$email'";
+                    $run_out = mysqli_query($connect, $str_out_users);
+                    $num_out = mysqli_num_rows($run_out);
+                    $str_add_users = "INSERT INTO `users` (`first_name`, `last_name`, `patronymic`, `email`, `password`, `phone`) VALUES ('$firstname', '$lastname', '$patronymic', '$email', '$password', '$phone')";
+
+                    if ($_SESSION['user']['role'] == '1') {
+                        header("Location:pages/lk_user.php");
+                    }elseif ($_SESSION['user']['role'] == '2') {
+                        header("Location:pages/lk_admin.php");
+                    }else {
+                        session_unset();
+                        if ($reg) {
+                            if ($num_out == 0) {
+                                if ($password == $replypassword) {
+                                    $run_add_users = mysqli_query($connect, $str_add_users);
+                                    if ($run_add_users) {
+                                        $str_out_user = "SELECT * FROM `users` WHERE email = '$email'";
+                                        $run_out_user = mysqli_query($connect, $str_out_user);
+                                        $user = mysqli_fetch_array($run_out_user);
+                                        $_SESSION['user'] = [
+                                            "id" => $user['id'],
+                                            "first_name" => $user['first_name'],
+                                            "last_name" => $user['last_name'],
+                                            "patronymic" => $user['patronymic'],
+                                            "email" => $user['email'],
+                                            "password" => $user['password'],
+                                            "phone" => $user['phone'],
+                                            "role" => $user['role'],
+                                            "role_sales" => $user['role_sales']
+                                        ];
+                                        if ($_SESSION['user']['role'] == '1') {
+                                            header("Location:pages/lk_user.php");
+                                        }elseif ($_SESSION['user']['role'] == '2') {
+                                            header("Location:pages/lk_admin.php");
+                                        }
+                                        
+                                    }
+                                    else {
+                                        echo "<p class='error error_auth'>Возникла ошибка!</p>";
+                                    }
+                                }
+                                else {
+                                    echo "<p class='error error_auth'>Пароли не совпадают!</p>";
+                                }
+                            }
+                            else {
+                                echo "<p class='error error_auth'>Пользователь с таким email уже существует!</p>";
+                            }
+                        }
+                    }
+                ?>
+            </form>
+            </div>
+            </div>
+        </div>
+    </div>
 	<div class="content">
 		<h2>Наши преимущества</h2>
 		<div class="preim">
@@ -47,36 +197,21 @@
 				<p>Вы можете настроить свой перелет так, как вам хочется, есть гибкая настройка перелета</p>
 			</div>
 		</div>
-		<h2>Отзывы</h2>
+		<h2 class="e-content_">Отзывы</h2>
 		<div class="otzivi">
-			<div class="otziv1">
-				<div class="menchik">
-					<img src="img/ava.png">
-					<p>Серега</p>
-				</div>
-				<p>Нормис</p>
-			</div>
-			<div class="otziv2">
-				<div class="menchik">
-					<img src="img/ava.png">
-					<p>Артем</p>
-				</div>
-				<p>Нормис</p>
-			</div>
-			<div class="otziv3">
-				<div class="menchik">
-					<img src="img/ava.png">
-					<p>Валя</p>
-				</div>
-				<p>Нормис</p>
-			</div>
-			<div class="otziv4">
-				<div class="menchik">
-					<img src="img/ava.png">
-					<p>Игорь</p>
-				</div>
-				<p>Нормис</p>
-			</div>
+		<?php
+                $str_out_reviews = "SELECT * FROM `reviews`WHERE `status` = 2 ORDER BY `id` DESC LIMIT 0, 4 ";
+                $run_out_reviews = mysqli_query($connect, $str_out_reviews);
+                while ($reviews = mysqli_fetch_array($run_out_reviews)) {
+                    $str_out_reviews_user = "SELECT * FROM `users` WHERE `id` = $reviews[id_user]";
+                    $run_out_rewiews_user = mysqli_query($connect, $str_out_reviews_user);
+                    $users_reviews = mysqli_fetch_array($run_out_rewiews_user);
+                    echo "<div class='e-reviews index_reviews'>
+                        <div class='e-name'><p>$users_reviews[first_name]</p></div>
+                        <div class='e-content'><p>$reviews[content]</p></div>
+                        </div>";
+                }
+        ?>
 		</div>
 	</div>
 	<div class="footer">
