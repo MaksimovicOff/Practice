47e8b76fc314fb932f8a18312a68fd8ccb80cfbf
J4trie <120220993+J4trie@users.noreply.github.com>
2024-02-15 17:14:09 +0400

    Create footer.php

---
+++ controllers/footer.php
@@ -0,0 +1,17 @@
+<div class="footer">
+	<div class="line">
+		<div class="logo_f"></div>
+		<div class="soc">
+			<div class="vk"><a href="#"></a></div>
+			<div class="tg"><a href="#"></a></div>
+		</div>
+	</div>
+	<div class="menu_f">
+		<a href="">Главная</a>
+		<a href="">Отмененнные рейсы</a>
+		<a href="">Расписание</a>
+		<a href="">Отзывы</a>
+		<a href="">О нас</a>
+		<a href="">Войти</a>
+	</div>
+</div>
