1a5a92739838b8555b86ef7ee833780874227634
maksmaksimovic <maksmaksimovic2004@gmail.com>
2024-02-15 21:10:49 +0400

    Update footer.php

--- controllers/footer.php
+++ controllers/footer.php
@@ -11,7 +11,16 @@
 		<a href="">Отмененнные рейсы</a>
 		<a href="">Расписание</a>
 		<a href="">Отзывы</a>
-		<a href="">О нас</a>
-		<a href="#modal" class="lk_f_logo">Войти</a>
+		<a href="../pages/about.php">О нас</a>
+		<?php
+        session_start();
+        include_once 'db/db.php';
+        error_reporting(0);
+        if ($_SESSION['user']) {
+        ?><a href="pages/lk_admin.php" class="lk_logo"><?php echo $_SESSION['user']['first_name']; ?></a><?php
+        }else {
+        ?><a href="#modal" class="lk_f_logo">Войти</a><?php
+        }
+        ?>	
 	</div>
 </div>
