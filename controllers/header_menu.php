733743ca7c9395d4dff4798aaec3adb220b691a4
maksmaksimovic <maksmaksimovic2004@gmail.com>
2024-02-15 20:28:30 +0400

    Create header_menu.php

---
+++ controllers/header_menu.php
@@ -0,0 +1,11 @@
+<div class="header">
+	<div class="logo"></div>
+	<div class="menu">
+		<a href="">Главная</a>
+		<a href="">Отмененнные рейсы</a>
+		<a href="">Расписание</a>
+		<a href="">Отзывы</a>
+		<a href="">О нас</a>
+		<a href="#modal" class="lk_logo">Войти</a>
+	</div>
+</div>
