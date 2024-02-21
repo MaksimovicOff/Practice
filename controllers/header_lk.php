bd14cef66e46e826cee2cf99716686331f906576
maksmaksimovic <maksmaksimovic2004@gmail.com>
2024-02-21 11:17:02 +0400

    надо доделать в лк хэдер ссылкам text-decoration не применяется и актив линк хз как делать

--- controllers/header_lk.php
+++ controllers/header_lk.php
@@ -1,5 +1,11 @@
-<div class="headerlk">
-	<div class="menu">
-        <a href="exit.php" class="exit_lk"><span><img src="../img/exit.png" alt="">Выйти</span></a>	
-	</div>
+<div class="header">
+    <div class="menu">
+        <a href="../index.php" class="menu_lk_links">Главная</a>
+        <a href="" class="menu_lk_links">Отмененнные рейсы</a>
+        <a href="" class="menu_lk_links">Расписание</a>
+        <a href="" class="menu_lk_links">Отзывы</a>
+        <a href="../pages/about.php" class="menu_lk_links">О нас</a>
+        <a href="exit.php" class="exit_lk"><span><i class="fas fa-sign-out-alt"></i> Выйти</span></a>  
+    </div>
 </div>
+<script src="https://kit.fontawesome.com/d1cb775d62.js" crossorigin="anonymous"></script>
